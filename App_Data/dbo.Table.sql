bc3783fe79d8e103a272a5a60bfe58e774403141
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-03 06:28:45 +0530

    Add project files.

---
+++ App_Data/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [stupic] VARCHAR(MAX) NULL, 
+    [stuname] VARCHAR(MAX) NULL, 
+    [fname] VARCHAR(MAX) NULL, 
+    [class] VARCHAR(MAX) NULL, 
+    [emailad] VARCHAR(MAX) NULL, 
+    [phone] VARCHAR(MAX) NULL, 
+    [gender] VARCHAR(MAX) NULL, 
+    [address] VARCHAR(MAX) NULL, 
+    [dbr] VARCHAR(MAX) NULL, 
+    [password] VARCHAR(MAX) NULL
+)
